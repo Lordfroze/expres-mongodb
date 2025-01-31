81050dabdff9a7f1d70acad26419b46b42144869
Lordfroze <yogaadiprtama22@gmail.com>
2025-01-31 14:37:55 +0700

    update|

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+// Import Models
+const Product = require('./models/product')
+
 // connect to mongodb
 mongoose.connect('mongodb://localhost:27017/shop_db').then((result) => {
     console.log('connected to mongodb')
@@ -15,6 +18,12 @@ app.set('view engine', 'ejs');  // menentukan engine yang digunakan
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+// Membuat Restful Index Product / menampilkan seluruh data index
+app.get('/products', async (req,res) => {
+    const products = await Product.find({})
+    res.render('products/index', {products})
 })
 
 app.listen(3000, () => {
